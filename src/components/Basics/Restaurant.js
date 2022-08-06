c9c14c78a5b7385a1c2b61e62b4e641f1e009a51
durveshramane26 <durveshramane6@gmail.com>
2022-08-06 23:56:34 +0530

    updated background

---
+++ src/components/Basics/Restaurant.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./style.css";
+const Restaurant = () => {
+  return (
+    <>
+      <div className="card-container">
+        <div className='card'>
+            <div className="card-body">
+                <span className='card-number card-cricle subtle' >1</span>
+            </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Restaurant

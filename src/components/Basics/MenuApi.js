a0d60bb8faded3d863f68c51193585c8916e1a72
durveshramane26 <durveshramane6@gmail.com>
2022-08-07 16:20:08 +0530

    Images added

---
+++ src/components/Basics/MenuApi.js
@@ -0,0 +1,11 @@
+const Menu = [{
+    id: 1,
+    image:"durvesh",
+    name: "maggie",
+    category: "lunch",
+    price: 25,
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam error non sunt provident accusamus cum iste? Esse, consectetur aperiam eos praesentium dolore quia minima ut, sit fugit dolor laborum quaerat.",
+},
+];
+
+export default Menu;

cc4a3ddacf543b96c21ac1e44da60f185a4c5bc5
durveshramane26 <durveshramane6@gmail.com>
2022-08-07 21:15:55 +0530

    added menuCard

---
+++ src/components/Basics/MenuCard.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const MenuCard = () => {
+  return (
+    <>
+        <div className="card-container">
+        <div className='card'>
+            <div className="card-body">
+                <span className='card-number card-cricle subtle' >1</span>
+                <span className='card-author subtle'>Breakfast</span>
+                <h2 className='card-title'> Maggie </h2>
+                <span className="card-description subtle"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam error non sunt provident accusamus cum iste? Esse, consectetur aperiam eos praesentium dolore quia minima ut, sit fugit dolor laborum quaerat. 
+                 </span>
+                 <div className="card-read">Read</div>
+            </div>
+            {/* <img src={image} alt="images" className="card-media" /> */}
+
+            <span className='card-tag subtle'>Order Now</span>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default MenuCard
